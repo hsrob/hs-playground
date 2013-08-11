40648d756e101368cf7f322ab45489924fb572bb
hsrob <whatman@gmail.com>
2013-08-10 18:58:30 -0700

    Add completed code (end of demo)

--- app/Assets/js/angular_apps/demo/ng-demo2.js
+++ app/Assets/js/angular_apps/demo/ng-demo2.js
@@ -16,9 +16,18 @@
     .controller('todoListCtrl', ['$scope', function ($scope) {
         //Initialize the todoList scope variable as an array
         $scope.todoList = [];
-        $scope.someScopeVar = '';
-        $scope.alertValue = function(){
-          alert($scope.someScopeVar + ' is set, and is also awesome.');
+        $scope.reverseList = [];
+        $scope.newTodo = '';
+
+        $scope.addTodoItem = function(){
+              $scope.todoList.push({
+                 Id: 1000,
+                  Title: $scope.newTodo,
+                  DateAdded: moment(),
+                  DateDue: moment().add('hours', 2)
+              });
+
+                $scope.newTodo = '';
         };
         var dateNow = new Date();
         
@@ -44,39 +53,20 @@
             }];
 
             _.each(wsResults, function(val) { $scope.todoList.push(val); });
+
+            var copyList = angular.copy($scope.todoList);
+            $scope.reverseList = copyList.reverse();
         };
     }])
-    //provide an editor input with autocomplete for each permission grant type (Level, Scope, Flags)
     .directive('todoItem', function () {
         return {
             restrict: 'E',
+            transclude: true,
             scope: {
-                //The binding for an ng-repeated PermissionGrant (ex. grnt)
-                itemName: '='
+                todolist: '='
             },
-            template:
-                '<div class="panel panel-success"><div class="panel-heading panel-heading-compact">Assigned</div>' +
-                    '<div class="tags">' +
-                        '<a ng-repeat="(idx, permLvl) in assignedPermType" class="tag" ng-click="remove(permLvl)">{{permLvl}}&nbsp;&times;</a>' +
-                    '</div>' +
-                '</div>' +
-                '<div class="panel" ng-show="availablePermType.length"><div class="panel-heading panel-heading-compact">Available</div>' +
-                    '<div class="tags">' +
-                        '<a ng-repeat="(idx, permLvl) in availablePermType" class="tag" ng-click="add(permLvl)">{{permLvl}}&nbsp;&plus;</a>' +
-                    '</div>' +
-                '</div>',
+            template: '<ul><li ng-repeat="todo in todolist">{{todo.Id}} {{todo.Title}}</li></ul><div ng-transclude></ng-transclude>',
             link: function (scope, elem, attrs) {
-                scope.remove = function (elValue) {
-                    scope.assignedPermType = _.without(scope.assignedPermType, elValue);
-                    scope.availablePermType.push(elValue);
-                };
-                scope.add = function (elValue) {
-                    scope.availablePermType = _.without(scope.availablePermType, elValue);
-                    scope.assignedPermType.push(elValue);
-                };
-                scope.$watch('itemName', function (newVal, oldVal) {
-                    scope.grantBinding[attrs.grantStringType] = _(newVal).toString();
-                });
             }
         };
     });
